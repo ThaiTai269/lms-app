74c013e01c8ac2517900a5995ae38095aa03154d
ThaiTai269 <89332731+ThaiTai269@users.noreply.github.com>
2022-01-18 17:05:06 +0700

    up

--- src/component/home/sidebar/sidebar.js
+++ src/component/home/sidebar/sidebar.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
 import logo from "../img/Logo1.png";
-import money from "../img/money1.png"
-import basicMoney from "../img/money.png"
-import forder from "../img/folder check.png"
+import money_sidebar from "../img/money1.png"
+import basicMoney_sidebar from "../img/money.png"
+import folder_sidebar from "../img/folder check.png"
+import money_nav from "../img/money3.png"
+import basicMoney_nav from "../img/money2.png"
+import folder_nav from "../img/folder2.png"
 
 
 
@@ -13,33 +16,54 @@ export default class Sidebar extends Component {
       <div className="navbar-content">
         <img className="logo-nav" src={logo} alt="logo"/>
         <ul className="navbar-icons">
-                <li className="icon-sidebar"><a><img src={money} alt="money"/></a></li>
-                <li className="icon-sidebar"><img src={basicMoney} alt="basicMoney"/></li>
-                <li className="icon-sidebar"><img src={forder} alt="forder"/></li>
+                <li className="icon-sidebar"> <a href="#"> <img className="icon-sidebar-img"  src={money_sidebar} alt="money"/></a></li>
+                <li className="icon-sidebar"> <a href="#"> <img className="icon-sidebar-img" src={basicMoney_sidebar} alt="basicMoney"/> </a> </li>
+                <li className="icon-sidebar"> <a href="#"> <img className="icon-sidebar-img" src={folder_sidebar} alt="forder"/> </a> </li>
         </ul>
       </div>
       <div className="navbar-menu">
         <ul className="navbar-menu-list">
-          <li className="navbar-menu-title navbar-menu-title--active">
-            <span className="menu-title-content">
-              Quản lý học phí
-              <ul className="menu-title-list">
-                <li className="menu-title-item menu-title-item--active ">
-                  Danh sách lớp học
-                </li>
-                <li className="menu-title-item">Danh sách biểu phí</li>
-                <li className="menu-title-item">Danh sách khoản thu</li>
-                <li className="menu-title-item">Hình thức thu</li>
-                <li className="menu-title-item">Danh sách chế độ miễn giảm</li>
-                <li className="menu-title-item">Thu học phí</li>
+          <li className="navbar-menu-title navbar-menu-title__cost">
+            <a href="" className="img-icon_nav-list menu-title-content">
+            <img className="icon-nav" src={money_nav} alt="money"/>
+            Quản lý học phí
+            </a>
+            <ul className="menu-title-list">
+                {/* menu-title-item--active  */}
+               
+                <li className="menu-title-item"> <a href=""> Danh sách lớp học </a> </li>
+                <li className="menu-title-item"> <a href=""> Danh sách biểu phí</a></li>
+                <li className="menu-title-item"> <a href=""> Danh sách khoản thu</a></li>
+                <li className="menu-title-item"> <a href=""> Hình thức thu</a></li>
+                <li className="menu-title-item"> <a href=""> Danh sách chế độ miễn giảm</a></li>
+                <li className="menu-title-item"> <a href=""> Thu học phí</a></li>
               </ul>
-            </span>
           </li>
-          <li className="navbar-menu-title">
-            <span className="menu-title-content">Quản lý lương</span>
+          <li className="navbar-menu-title navbar-menu-title__wage">
+            
+            <a href="" className="img-icon_nav-list menu-title-content">
+            <img className="icon-nav" src={basicMoney_nav} alt="money"/>
+            Quản lý Lương
+            </a>
+            <ul className="menu-title-list ">
+                {/* menu-title-item--active  */}
+               
+                <li className="menu-title-item"> <a href=""> Danh sách giáo viên</a> </li>
+                <li className="menu-title-item"> <a href=""> Danh sách bảng chấm công</a></li>
+                <li className="menu-title-item"> <a href=""> Danh sách bảng lương</a></li>
+                <li className="menu-title-item"> <a href=""> Danh sách biểu phí</a></li>
+                <li className="menu-title-item"> <a href=""> Danh sách khoản chi</a></li>
+                <li className="menu-title-item"> <a href=""> Thiết lập ngạch, hạng</a></li>
+                <li className="menu-title-item"> <a href=""> Công thức tính lương</a></li>
+
+              </ul>
           </li>
-          <li className="navbar-menu-title">
-            <span className="menu-title-content">Đề xuất và phê duyệt</span>
+
+            <li className="navbar-menu-title">
+            <a href="" className="img-icon_nav-list menu-title-content">
+            <img className="icon-nav" src={folder_nav} alt="money"/>
+            Đề xuất và phê duyệt
+            </a>
           </li>
         </ul>
       </div>
