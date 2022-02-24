e830c53b918a27f16bc0ea82d31aed2e5989f106
ThaiTai269 <89332731+ThaiTai269@users.noreply.github.com>
2022-02-24 17:33:17 +0700

    up

--- src/component/home/content/modal/modal_receivables/modal_receivables.tsx
+++ src/component/home/content/modal/modal_receivables/modal_receivables.tsx
@@ -9,16 +9,36 @@ function Modal_receivables ({ setIsOpen_modal_reaceivables } : {setIsOpen_modal_
 
             </div>
             <div className="receivables_body">
+                 <div className="receivables_body_control">
+                    <img src={cancel} alt="" className="receivables_body_control_icon"onClick={() => setIsOpen_modal_reaceivables(false)}/>
+                </div>
                 <div className="receivables_body_content">
-                    <div className="receivables_body_control">
-                        <img src={cancel} alt="" className="receivables_body_control_icon"onClick={() => setIsOpen_modal_reaceivables(false)}/>
-                    </div>
+                    
                     <div className="receivables_body_code">
-                        <p className="receivables_body_code_name">Số phiếu:</p>
-                        <p className="receivables_body_code_number">121235</p>
+                        <div className="receivables_body_code_item">
+                            <p className="receivables_body_code_name">Số phiếu:</p>
+                            <p className="receivables_body_code_number">121235</p>
+                        </div>
+                        <div className="receivables_body_code_item">
+                            <p className="receivables_body_code_name">Mã hóa đơn:</p>
+                            {/* <p className="receivables_body_code_number">121235</p> */}
+                        </div>
                     </div>
                     <div className="receivables_body_table">
+                        <div className="receivables_body_table_heading">
+                            <div className="receivables_body_table_heading_item">
+                                <div className="receivables_table_heading_order">STT</div>
+                                <div className="receivables_table_heading_code">Mã </div>
+                                <div className="receivables_table_heading_content">Nội dung thu</div>
+                                <div className="receivables_table_heading_semester">Học kỳ</div>
+                                <div className="receivables_table_heading_price">Số tiền (VNĐ)</div>
+                            </div>
+                            
+                        </div>
 
+                        <div className="receivables_body_table_body">
+
+                        </div>
                     </div>
                 </div>
                
